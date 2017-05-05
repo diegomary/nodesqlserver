d42166cabe563ee8591dae1343bd49c8d9aed6aa
Diego <diego@dmm888.com>
2017-05-05 19:08:38 +0100

    added some logic and database of example to restore in sqlexpress

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var config = {
     user: 'sa',
     password: 'diegomary',
     server: 'localhost\\SQLEXPRESS',
-    database: 'netcore'
+    database: 'northwind'
 };
 
 app.get('/', function (req, res, next) {
@@ -16,17 +16,33 @@ app.get('/', function (req, res, next) {
     sql.connect(config,  (err) => {
 
         if (err) console.log(err);
-        // create Request object
-        var request = new sql.Request();
+        let request = new sql.Request();   
+        request.query(`select distinct Orders.OrderID from Orders`, (err, rsorders) => {
+                if (err) console.log(err)
 
-        // query to the database and get the records
-        request.query('select * from candidates', (err, recordset) => {
+                request.query(`select Orders.OrderID, Orders.OrderDate, orders.ShipCity,[Order Details].Quantity,
+                                [Order Details].ProductID,products.ProductName, Products.UnitPrice from
+                                orders inner join  [Order Details] on orders.orderid=[Order Details].orderid
+                                inner join Products on [Order Details].ProductID = Products.ProductID;`, (err, rs) => {
+                    if (err) console.log(err);
+                    let oId = rsorders.recordset;
+                    //let order = {};
+                    for (let id of oId) {
 
-            if (err) console.log(err)
+                        let documents = rs.recordset.filter(function (doc) {
+                            return (doc.OrderID === id.OrderID);
+                        });
 
-            // send records as a response
-            res.send(recordset.recordset);
-            sql.close();
+                        id.orderdetails = documents;
+                       // console.log(id.OrderID);                   
+
+                    }
+                    //console.log(rsorders);
+                    res.send(oId);                    
+                    sql.close();
+                } )              
+
+           
         })
     })
   
